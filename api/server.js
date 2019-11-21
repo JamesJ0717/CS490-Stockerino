ef25cc39570f7b29826babfbab3881e00ca03135
James Johnson <me@jamesjohnson.io>
2019-11-21 16:12:59 -0500

    removed api

--- api/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const app = require('./app');
-
-const port = process.env.port || 3000;
-
-const server = http.createServer(app.app);
-console.log('Server listening on port', port);
-server.listen(port);
